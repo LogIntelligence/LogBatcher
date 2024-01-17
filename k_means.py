1c9e803cf5a0c557aca10fb2ee574f6bca7e77d8
xiaoyi <1802123579@qq.com>
2024-01-17 21:19:32 +0800

    '1/17'

--- k_means.py
+++ k_means.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from concurrent.futures import ThreadPoolExecutor
 import os
 import random
 import time
@@ -10,33 +11,12 @@ from openai import OpenAI
 import openai
 import backoff
 import httpx
+from tqdm import tqdm
 from post_process import correct_single_template
 
-api_key = "sk-KlLjOnG8myjg3Hhg5dF71699E6814e8b9753F00bB076C400"
-client = OpenAI(
-    base_url="https://oneapi.xty.app/v1",  # 中转url
-    api_key=api_key,                      # api_key
-    http_client=httpx.Client(
-        proxies="http://127.0.0.1:7890"  # 代理地址
-    ),
-)
 
-# instruction_noindex = '''You will be provided with some log messages. You should check whether the giving log messages share the same template. If so, abstract variables with ‘{placeholders}’ and return the template without additional explatation, otherwise return 'false' only.'''
-
-instruction_noindex = '''You will be provided with some log messages. You should check whether the giving log messages share the same template. If so, abstract variables with `{placeholders}` and return the template without additional explatation'''
-
-instruction_new = '''You will be provided with some log messages. You should check if the giving log messages share the same template. If so, abstract variables with `{{placeholders}}` to extract the corresponding template.
-Print the input log's template delimited by backticks.
-'''
-
-instruction_for_one_log = '''You will be provided with a log message delimited by backticks. You must abstract variables with `{{placeholders}}` to extract the corresponding template.
-Print the input log's template delimited by backticks.'''
-
-instruction2 = '''Giving some log tempaltes, the AI assistant should merge the possibly same templates'''
-
-
-def tokenize(log_content):
-    words = re.split('[ ,]', log_content)
+def tokenize(log_content, tokenize_pattern=r'[#= ,]'):
+    words = re.split(tokenize_pattern, log_content)
     # words = log_content.split()
     words = [word for word in words if not re.search(r'\d', word)]
     return words
@@ -47,39 +27,111 @@ def vectorize(tokenized_logs):
     return vectorizer.fit_transform(tokenized_logs)
 
 
-def cluster(vectorized_logs, num_clusters = '10', method='kmeans'):
-    if method == 'kmeans':
-        kmeans = KMeans(n_clusters=num_clusters)
-        kmeans.fit(vectorized_logs)
-        return kmeans.labels_
-    if method == 'dbscan':
-        dbscan = DBSCAN(eps=0.3, min_samples=5)
-        dbscan.fit(vectorized_logs)
-        return dbscan.labels_
+def cluster(vectorized_logs, num_clusters='10', cluster_method='kmeans'):
+    if cluster_method == 'kmeans':
+        cluster = KMeans(n_clusters=num_clusters)
+    if cluster_method == 'dbscan':
+        cluster = DBSCAN(eps=0.3, min_samples=5)
+    cluster.fit(vectorized_logs).fit(vectorized_logs)
+    labels = cluster.labels_
+    cluster_nums = max(labels) + 1
+    return labels, cluster_nums
+    
+
+def reassign_clusters(labels, cluster_nums, tokenized_logs):
+    mergerd_logs = []
+    for tokenized_log in tokenized_logs:
+        mergerd_logs.append(' '.join(tokenized_log))
+
+    for i in range(len(labels)):
+        if labels[i] == -1:
+            for j in range(i+1, len(labels)):
+                if labels[j] == -1 and mergerd_logs[i] == mergerd_logs[j]:
+                    labels[j] = cluster_nums
+            labels[i] = cluster_nums
+            cluster_nums += 1
+    return labels, cluster_nums
+
+class Parser:
+    def __init__(self, api_key, model='gpt-3.5-turbo', using_proxy=True, cluster_method='dbscan', batch_num = 50):
+        self.api_key = api_key
+        self.model = model
+        self.cluster_method = cluster_method
+        self.batch_num = batch_num
+        self.random = True
+        self.instruction_batch = '''You will be provided with some log messages. You should check if the giving log messages share the same template. If so, abstract variables with `{{placeholders}}` to extract the corresponding template.
+        Print the input log's template delimited by backticks.'''
+        self.instruciton_one_log = '''You will be provided with a log message delimited by backticks. You must abstract variables with `{{placeholders}}` to extract the corresponding template.
+        Print the input log's template delimited by backticks.'''
+        if using_proxy:
+            self.client = OpenAI(
+                base_url="https://oneapi.xty.app/v1",  # 中转url
+                api_key=api_key,                      # api_key
+                http_client=httpx.Client(
+                    proxies="http://127.0.0.1:7890"  # 代理地址
+                ),
+            )
+        else:
+            self.client = OpenAI(
+                base_url="https://oneapi.xty.app/v1", api_key=api_key)
+
+    @backoff.on_exception(backoff.expo, (openai.APIStatusError, openai.InternalServerError), max_tries=5)
+    def chat(self, messages):
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=messages,
+            temperature=0.0,
+        )
+        return response.choices[0].message.content.strip('\n')
+    
+    def get_responce(self, f, input):
+        label,  logs, indexs, ground_truth = input
+        length = len(indexs)
+        templates = []
+        if self.random:
+            seed = time.time()
+            random.seed(seed)
+            random.shuffle(logs)
+
+        for i in range(0, len(logs), self.batch_num):
+            batch_logs = logs[i:i+self.batch_num]
+            # if all logs's length is 1, and not contain any digit, return the log itself
+            if all(len(re.split(' ', log)) == 1 and not any(char.isdigit() for char in log[0]) for log in batch_logs):
+                return batch_logs[0]
+
+            messages = []
+            if len(batch_logs) == 1:
+                messages.append({"role": "system", "content": self.instruciton_one_log})
+            else:
+                messages.append({"role": "system", "content": self.instruction_batch})
+            # batch logs to str
+            prompt = ""
+            for log in batch_logs:
+                prompt += log + '\n'
+            messages.append({"role": "user", "content": prompt.strip('\n')})
+            answer = self.chat(messages)
+            template =  postprocessing(answer , isafter=False)
+            if template != '':
+                templates.append(template)
+
+            f.write(f"---------------------------\n")
+            f.write(f"cluster {label}: len={length}\n")
+            f.write(f"{ground_truth} (ground truth)\n")
+            print(f"cluster {label}: len={length}")
+
+            final_tempalte, freq = choose(templates)
+                
+            # 打印结果
+            for key, value in freq.items():
+                f.write(f"{key}: {value}\n")
+                print(f"{key}: {value}")
+            f.write(f"---------------------------\n")
+            return final_tempalte
 
 
-@backoff.on_exception(backoff.expo, (openai.APIStatusError, openai.InternalServerError), max_tries=5)
-def chat(messages):
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-    )
-    return response.choices[0].message.content.strip('\n')
-
-# 预处理
 
 
 def postprocessing(response, isafter = False):
-    boolean = {'true', 'false'}
-    default_strings = {'null', 'root', 'admin'}
-    path_delimiters = {  # reduced set of delimiters for tokenizing for checking the path-like strings
-        r'\s', r'\,', r'\!', r'\;', r'\:',
-        r'\=', r'\|', r'\"', r'\'',
-        r'\[', r'\]', r'\(', r'\)', r'\{', r'\}'
-    }
-    token_delimiters = path_delimiters.union({  # all delimiters for tokenizing the remaining rules
-        r'\.', r'\-', r'\+', r'\@', r'\#', r'\$', r'\%', r'\&',
-    })
 
     response = response.strip().strip('\n')
     if "\n\n" in response:
@@ -99,6 +151,8 @@ def postprocessing(response, isafter = False):
     if not isafter:
         template = correct_single_template(template)
     if isafter:
+        boolean = {'true', 'false'}
+        default_strings = {'null', 'root', 'admin'}
         tokens = template.split(' ')
         for i in range(len(tokens)):
             if re.match(r'^\d+$', tokens[i]):
@@ -119,27 +173,12 @@ def postprocessing(response, isafter = False):
         template = ' '.join(tokens)
     return template
 
-
-def batch_parsing(batch_logs):
-
-    # if all logs's length is 1, and not contain any digit, return the log itself
-    if all(len(log) == 1 and not any(char.isdigit() for char in log[0]) for log in batch_logs):
-        return batch_logs[0]
-
-    messages = []
-    messages.append({"role": "system", "content": instruction_new})
-    # batch logs to str
-    prompt = ""
-    for log in batch_logs:
-        prompt += log + '\n'
-    messages.append({"role": "user", "content": prompt})
-    answer = chat(messages)
-    return postprocessing(answer , isafter=False)
+    
 
 
 def choose(templates):
 
-    # 使用Counter计算频率
+    # majority vote
     freq = Counter(templates)
     length = len(freq) 
     candidates = freq.most_common(len(freq))
@@ -161,66 +200,49 @@ def choose(templates):
             final_template = candidates[1][0]
     return final_template, freq
 
-def get_responce(f, indexs, label, logs_temp, k, ground_truth, israndom=True):
-    length = len(indexs)
-    templates = []
-    if israndom:
-        seed = time.time()
-        random.seed(seed)
-        random.shuffle(indexs)
-        random.seed(seed)
-        random.shuffle(logs_temp)
-
-    for i in range(0, len(logs_temp), k):
-        batch = logs_temp[i:i+k]
-        template = batch_parsing(batch)
-        if template != '':
-            templates.append(template)
-
-    f.write(f"---------------------------\n")
-    f.write(f"cluster {label}: len={length}\n")
-    f.write(f"{ground_truth} (ground truth)\n")
-    print(f"cluster {label}: len={length}")
-
-    output_tempalte, freq = choose(templates)
-        
-    # 打印结果
-    for key, value in freq.items():
-        f.write(f"{key}: {value}\n")
-        print(f"{key}: {value}")
-    f.write(f"---------------------------\n")
-    return indexs, output_tempalte
-
 
 def single_dataset_paring(dataset, output_dir, k = 10, cluster_method='kmeans'):
-    os.makedirs(output_dir, exist_ok=True)
-    # 读取日志文件
-    df = pd.read_csv(
-        f'dataset/{dataset}/{dataset}_2k.log_structured_corrected.csv')
+
+    parser = Parser(
+        api_key='sk-vsxZyOrNwf6cmjPl7f4c7e05DcA8433dAa4e73AcD1E89610')
+    
+    # load dataset
+    df = pd.read_csv(f'dataset/{dataset}/{dataset}_2k.log_structured_corrected.csv')
     logs = df['Content'].tolist()
 
-    # 对logs分词，得到logs_label，二维列表
+    # tokenize
     tokenized_logs = [tokenize(log) for log in logs]
     
+    # cluster 1st
+    labels, cluster_nums = cluster(vectorize(tokenized_logs), k, cluster_method)
+    
+    # cluster 2nd
+    labels, cluster_nums = reassign_clusters(labels, cluster_nums, tokenized_logs)
 
-    # 聚类
-    labels = cluster(vectorize(tokenized_logs), k, cluster_method)
-    cluster_nums = max(labels) + 1
-    logs_label = []
-    for i in range(cluster_nums):
-        logs_label.append([])
-    for i, label in enumerate(labels):
-        if label != -1:
-            logs_label[label].append(i)
+    # output file
+    os.makedirs(output_dir, exist_ok=True)
+    f = open(output_dir + f'{dataset}.txt', 'w')
 
-    f = open(output_dir+ f'{dataset}.txt', 'w')
     outputs = [None for _ in range(2000)]
-    for label, indexs in enumerate(logs_label):
-        logs_temp = [logs[i] for i in indexs]
-        ground_truth = df['EventTemplate'][indexs[0]]
-        indexs_after, template = get_responce(
-            f, indexs, label, logs_temp, 50, ground_truth)
-        for index in indexs_after:
+    
+    inputs = []
+    for i in range(cluster_nums):
+        inputs.append([-1, [], [], '']) # label, logs, indexs, ground_truth
+    for i, label in enumerate(labels):
+        inputs[label][0] = label
+        inputs[label][1].append(logs[i])
+        inputs[label][2].append(i)
+        if inputs[label][3] == '':
+            inputs[label][3] = df['EventTemplate'][i]
+
+    # with ThreadPoolExecutor(max_workers=16) as executor:
+    #     templates = list(
+    #         tqdm(executor.map(lambda chunk: chat.get_response(chunk, request_type=MSG_LEN == 1), log_chunks),
+    #              total=len(log_chunks)))
+            
+    for label in range(cluster_nums):
+        template = parser.get_responce(f, inputs[label])
+        for index in inputs[label][2]:
             outputs[index] = template
 
     f.close()
@@ -233,12 +255,11 @@ def single_dataset_paring(dataset, output_dir, k = 10, cluster_method='kmeans'):
 if __name__ == "__main__":
     datasets = ['BGL', 'HDFS', 'Linux', 'HealthApp', 'OpenStack', 'OpenSSH', 'Proxifier', 'HPC', 'Zookeeper', 'Mac',
                 'Hadoop', 'Android', 'Windows', 'Apache', 'Thunderbird', 'Spark']
-    datasets = ['Linux', 'HealthApp', 'OpenStack', 'Proxifier', 'HPC', 'Zookeeper', 'Mac',
-                'Hadoop', 'Android', 'Windows', 'Thunderbird', 'Spark']
+    datasets = ['Apache']
     cluster_nums = [132, 14, 143, 71, 56, 180, 14, 51, 54, 350, 115, 189, 57, 6, 194, 38]
                  # [120, 14, 116, 75, 43,  26,  8, 46, 50, 341, 114, 158, 50, 6, 149, 36]
-    output_dir = 'outputs/k_means/Second/'
-    for index, dataset in enumerate(datasets[:2]):
+    output_dir = 'outputs/k_means/Fourth/'
+    for index, dataset in enumerate(datasets):
         k = cluster_nums[index]
         single_dataset_paring(dataset, output_dir, cluster_method='dbscan',
                               )
