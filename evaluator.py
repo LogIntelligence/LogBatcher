e11987e729492dbc67a143d80b61afdfc2f08adf
xiaoyi <1802123579@qq.com>
2024-01-21 20:22:44 +0800

    '1/21-1'

--- evaluator.py
+++ evaluator.py
@@ -62,23 +62,23 @@ def get_accuracy(series_groundtruth, series_parsedlog, debug=False):
         logIds = series_parsedlog[series_parsedlog == parsed_eventId].index
         series_groundtruth_logId_valuecounts = series_groundtruth[logIds].value_counts(
         )
-        error_eventIds = (
-            parsed_eventId, series_groundtruth_logId_valuecounts.index.tolist())
-        error = True
+        # error_eventIds = (
+        #     parsed_eventId, series_groundtruth_logId_valuecounts.index.tolist())
+        # error = True
         if series_groundtruth_logId_valuecounts.size == 1:
             groundtruth_eventId = series_groundtruth_logId_valuecounts.index[0]
             if logIds.size == series_groundtruth[series_groundtruth == groundtruth_eventId].size:
                 accurate_events += logIds.size
                 error = False
-        if error and debug:
-            print('(parsed_eventId, groundtruth_eventId) =',
-                  error_eventIds, 'failed', logIds.size, 'messages')
-        for count in series_groundtruth_logId_valuecounts:
-            if count > 1:
-                accurate_pairs += scipy.special.comb(count, 2)
+        # if error and debug:
+        #     print('(parsed_eventId, groundtruth_eventId) =',
+        #           error_eventIds, 'failed', logIds.size, 'messages')
+        # for count in series_groundtruth_logId_valuecounts:
+        #     if count > 1:
+        #         accurate_pairs += scipy.special.comb(count, 2)
 
-    precision = float(accurate_pairs) / parsed_pairs
-    recall = float(accurate_pairs) / real_pairs
-    f_measure = 2 * precision * recall / (precision + recall)
+    # precision = float(accurate_pairs) / parsed_pairs
+    # recall = float(accurate_pairs) / real_pairs
+    # f_measure = 2 * precision * recall / (precision + recall)
     accuracy = float(accurate_events) / series_groundtruth.size
     return precision, recall, f_measure, accuracy
