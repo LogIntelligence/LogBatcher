3f0ca1db9f50971b9fa48934bf51255d3815db25
xiaoyi <1802123579@qq.com>
2024-03-18 18:55:20 +0800

    3/18

--- evaluator.py
+++ evaluator.py
@@ -44,9 +44,9 @@ def evaluate(file, dataset, mismatch=False):
     (precision, recall, f_measure, accuracy_PA) = get_accuracy(df['EventTemplate'],
                                                                df['Output'].apply(rule))
 
-    print(
-        'Precision: %.4f, Recall: %.4f, F1_measure: %.4f, Group Accuracy: %.4f, Message-Level Accuracy: %.4f, Edit Distance: %.4f' % (
-            precision, recall, f_measure, accuracy_PA, accuracy_exact_string_matching, edit_distance_result_mean))
+    # print(
+    #     'Precision: %.4f, Recall: %.4f, F1_measure: %.4f, Group Accuracy: %.4f, Message-Level Accuracy: %.4f, Edit Distance: %.4f' % (
+    #         precision, recall, f_measure, accuracy_PA, accuracy_exact_string_matching, edit_distance_result_mean))
     dataset = ' ' * (12 - len(dataset)) + dataset 
     print('%s: group Accuracy: %.4f, Message-Level Accuracy: %.4f, Edit Distance: %.4f' % (dataset, accuracy_PA, accuracy_exact_string_matching, edit_distance_result_mean))
     return accuracy_PA, accuracy_exact_string_matching, edit_distance_result_mean, edit_distance_result_std
